5d3fb6e0cd5cce92aad075210e505e19584c5599
arnav-snowleo <prakashsinghaarav@gmail.com>
2022-07-06 05:31:17 +0530

    searchbar ui done

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+
+import {Box, Stack, Typography, Button} from '@mui/material';
+
+import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx= {{mt: {lg: '212px' , xs: '70px'} , ml: {sm: '50px'}}} position="relative" p='20px'>
+      <Typography color="#FF2625" fontWeight='600' fontSize="26px">
+        Gym-go
+      </Typography>
+
+      <Typography fontWeight={700}  sx={{fontSize : {lg: '44px' , xs: '36px'} }} mb='23px' mt='30px' >
+        Sweat, Sweat <br/> Repeat
+      </Typography>
+
+      <Typography lineHeight='35px' fontWeight='200' fontSize="22px" mb={4}>
+        Check out the most effective Exercies
+      </Typography>
+      <Button href='#exercises' variant='contained' color='error' sx={{ backgroundColor: '#FF2625' , padding: '10px'}}>
+        Explore Exercies
+      </Button>
+      <Typography color="#FF2625" fontWeight={600} fontSize="150px" mb={3} sx={{
+        opacity: 0.1,
+        display: { lg: 'block' , xs: 'none'}
+      }}>
+       Gym-go
+      </Typography>
+      <img src = {HeroBannerImage}  alt='banner' className='hero-banner-img'/>
+    </Box>
   )
 }
 
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+
+import {Box, Stack, TextField, Typography, Button} from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems='center' mt='37px' justifyContent='center' p ='20px'>
+      <Typography fontWeight={700} sx ={{ fontsize: {lg: '44px' , xs: '30px'}}} mb='50px' textAlign='center'>
+        Awesome Exercises <br/>you should know
+      </Typography>
+      
+      <Box position='relative' mb='72px'>
+        <TextField sx={{input: {fontWeight: '700' , border: 'none' , borderRadius: '4px'} , width: {lg: '800px' , xs: '350px'}, backgroundColor: '#fff', borderRadius: '40px'}} height='76px' value='' onChange={(e) => {}  } type='text' placeholder='Search Exercises'
+
+        
+        />
+        <Button className= 'search-btn' sx={{
+          bgcolor: '#FF2625',
+          color: '#FFF',
+          textTransform: 'none',
+          width: {lg: '175px' , xs: '80px'},
+          fontSize: {lg: '20px' , xs: '14px'},
+          height: '56px',
+          position: 'absolute',
+          right: '0',  //important
+        }}>
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
