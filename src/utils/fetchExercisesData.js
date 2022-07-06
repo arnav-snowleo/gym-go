a0a05ae56bc8e812f67a45defb42ee381b1198d4
arnav-snowleo <prakashsinghaarav@gmail.com>
2022-07-06 06:22:50 +0530

    removed env file

--- src/utils/fetchExercisesData.js
+++ src/utils/fetchExercisesData.js
@@ -4,7 +4,7 @@ export const exerciseOptions = {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
-  };
+};
   
 
 export const fetchExercisesData = async(url, options) => {
