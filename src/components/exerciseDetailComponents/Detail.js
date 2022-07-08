64b3829db8bd15a6d2dcadcc2e7932a5082a3005
arnav-snowleo <prakashsinghaarav@gmail.com>
2022-07-08 16:13:39 +0530

    exercise detail done

--- src/components/exerciseDetailComponents/Detail.js
+++ src/components/exerciseDetailComponents/Detail.js
@@ -5,11 +5,30 @@ import {Typography, Stack , Button} from '@mui/material';
 import BodyPartImage from '../../assets/icons/body-part.png';
 import TargetImage from '../../assets/icons/target.png';
 import EquipmentImage from '../../assets/icons/equipment.png';
+import { width } from '@mui/system';
 
 const Detail = ({exerciseDetail}) => {
 
   // object de-structuring
   const {bodyPart, gifUrl, name , target, equipment} = exerciseDetail;
+
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      string: 'bodypart: ',
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      string: 'target: ',
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      string: 'equipments: ',
+      name: equipment,
+    },
+  ]
 
   return (
     <Stack gap='60px' sx={{
@@ -18,9 +37,35 @@ const Detail = ({exerciseDetail}) => {
       alignItems: 'center'
     }}>
       <img src={gifUrl} alt={name} loading='lazy' className ='detail-image'/>
-      <Typography ml='21px' pb='10px' color='#000' fontSize='20px' fontWeight = 'bold' mt='11px' textTransform='capitalize'>
+      <Stack sx={{
+        gap: {lg: '35px' , xs: '20px'}
+      }}>
+        <Typography ml='21px' pb='10px' color='#000' fontSize='40px' fontWeight = 'bold' mt='11px' textTransform='capitalize'>
             {name}
-      </Typography>
+        </Typography>
+        <Typography variant ='h6' ml='21px' pb='10px' color='#000' fontSize='20px' fontWeight = 'bold' mt='11px' textTransform='capitalize'>
+            {name} is done for {target}
+        </Typography>
+
+        {extraDetail.map((item) => (
+          <Stack key={item.name}  direction='row' gap='24px' alignItems='center'>
+            <Button sx={{
+              background: '#fff2db',
+              borderRadius: '50%',
+              width: '100px',
+              height: '100px',
+            }}>
+              <img src={item.icon} alt={bodyPart} style={{width:'50px', height:'50px'}}/>
+            </Button>
+            <Typography variant='h5'>
+              {item.string} {item.name}
+            </Typography>
+
+          </Stack>
+        ))}
+
+      </Stack>
+
 
     </Stack>
   )
